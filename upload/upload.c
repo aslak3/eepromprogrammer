5f6a7c419b866afe1194a853350172c7214c2d98
Lawrence Manning <lawrence@aslak.net>
2013-03-30 22:48:18 +0000

    Restructuring, and adding the ability to dump...
    the content, with an offset and count.

--- upload/upload.c
+++ upload/upload.c
@@ -12,39 +12,90 @@
 
 void stripnl(char *s);
 void checkfordone(char *command, char *replybuffer);
+FILE *openserialport(char *portdevice);
+void closeserialport(FILE *serialfp);
+int uploadfile(FILE *serialfp, char *filename);
+int readrawbytes(FILE *serialfp, int readrawcount, int offset);
+void usage(char *argv0);
 
 int main(int argc, char *argv[])
 {
-	char replybuffer[REPLY_SIZE];
+	char *argv0 = argv[0];
+	char *serialport = "/dev/ttyUSB0";
 	char *filename = NULL;
+	int readrawcount = 0;
+	int offset = 0;
+	int c;
 	
-	if (argc < 2)
+	while ((c = getopt(argc, argv, "hs:f:r:o:")) != -1)
 	{
-		fprintf(stderr, "Usage: %s filename\n", argv[0]);
-		exit(1);
+		switch(c)
+		{
+			case 'h':
+				usage(argv[0]);
+				break;
+			case 's':
+				serialport = optarg;
+				break;
+			case 'f':
+				filename = optarg;
+				break;
+			case 'r':
+				readrawcount = atol(optarg);
+				break;
+			case 'o':
+				offset = atol(optarg);
+				break;
+				
+			default:
+				abort();
+		}
 	}
-	
-	filename = argv[1];
 
-	int filefd = open(filename, O_RDONLY);
-	if (filefd < 0)
-	{
-		perror("Unable to open input file");
-		exit(1);
-	}
-	
-	struct stat statbuf;
-	fstat(filefd, &statbuf);
-	off_t filesize = statbuf.st_size;
-	
-	printf("File is %ld bytes\n", filesize);
-
-	int serialfd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
-	if (serialfd < 0)
+	FILE *serialfp = openserialport(serialport);
+	if (!serialfp)
 	{
 		perror("Unable to open serial port");
 		exit(1);
 	}
+	
+	if (filename)
+		uploadfile(serialfp, filename);
+	else if (readrawcount)
+		readrawbytes(serialfp, readrawcount, offset);
+	else
+	{
+		fprintf(stderr, "No filename specified\n");
+		usage(argv0);
+	}
+	
+	closeserialport(serialfp);
+	
+	return (0);
+}
+
+void stripnl(char *s)
+{
+	char *t = s;
+
+	while (*t != '\r' && *t != '\n') t++;
+	*t = '\0';
+}
+
+void checkfordone(char *command, char *replybuffer)
+{
+	if (strcmp(replybuffer, "Done") != 0)
+	{
+		fprintf(stderr, "Invalid response to %s command (%s)\n", command, replybuffer);
+		exit(1);
+	}
+}
+
+FILE *openserialport(char *portdevice)
+{
+	int serialfd = open(portdevice, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (serialfd < 0)
+		return NULL;
 
 	fcntl(serialfd, F_SETFL, 0);
 
@@ -56,18 +107,38 @@ int main(int argc, char *argv[])
 	options.c_cflag |= CS8;
 	tcsetattr(serialfd, TCSANOW, &options);
 
-	FILE *serialfp = fdopen(serialfd, "rw+");
-	if (!serialfp)
+	FILE *serialfp = fdopen(serialfd, "rw+");;
+
+	return serialfp;
+}
+
+void closeserialport(FILE *serialfp)
+{
+	fclose(serialfp);
+}
+
+int uploadfile(FILE *serialfp, char *filename)
+{
+	char replybuffer[REPLY_SIZE];
+
+	int filefd = open(filename, O_RDONLY);
+	if (filefd < 0)
 	{
-		perror("Unable to open serial port as file handle");
-		exit(1);
+		perror("Unable to open input file");
+		return 1;
 	}
+	
+	struct stat statbuf;
+	fstat(filefd, &statbuf);
+	off_t filesize = statbuf.st_size;
+	
+	printf("File is %ld bytes\n", filesize);
 
 	fprintf(serialfp, "reset\r");
 	fgets(replybuffer, REPLY_SIZE, serialfp);
 	stripnl(replybuffer);
 	checkfordone("reset", replybuffer);
-
+	
 	printf("Writing...\n");
 
 	off_t bytessent = 0; int blockssent = 0;
@@ -76,20 +147,13 @@ int main(int argc, char *argv[])
 	while (bytessent < filesize)
 	{
 		int bytesread;
-		int mode = 1;
-		
+
 		memset(transmissionbuffer, 0, 64);
-		if (bytessent == 8128 ||
-			bytessent == 16320 ||
-			bytessent == 960)
-		{
-			mode = 0;
-		}
-		fprintf(serialfp, "upblock 1 %d\r", mode);
+		fprintf(serialfp, "upblock 1 1\r");
 		if ((bytesread = read(filefd, transmissionbuffer, 64)) < 0)
 		{
 			perror("Unable to read from input file");
-			exit(1);
+			return 1;
 		}
 		fwrite(transmissionbuffer, 64, 1, serialfp);
 		fgets(replybuffer, 100, serialfp);
@@ -121,13 +185,13 @@ int main(int argc, char *argv[])
 		if (fread(&memorybyte, 1, 1, serialfp) < 1)
 		{
 			perror("Unable to read serial when validating");
-			exit(1);
+			return 1;
 		}
 		
 		if (read(filefd, &filebyte, 1) < 1)
 		{
 			perror("Unable to read file when validating");
-			exit(1);
+			return 1;
 		}
 		
 		if (memorybyte != filebyte)
@@ -135,7 +199,7 @@ int main(int argc, char *argv[])
 			printf("\n");
 			fprintf(stderr, "Error validating at byte %ld (%04lx), should be %02x but got %02x\n",
 				bytesvalidated, bytesvalidated, filebyte, memorybyte);
-			exit(1);
+			return 1;
 		}
 		
 		bytesvalidated++;
@@ -151,30 +215,50 @@ int main(int argc, char *argv[])
 	checkfordone("dumpraw", replybuffer);
 
 	printf("\n=== No errors ===\n");
-	
-	if (serialfp)
-		fclose(serialfp);
-	if (serialfd > -1)
-		close(serialfd);
+
 	if (filefd > -1)
 		close(filefd);
+
+	return 0;
+}
+
+int readrawbytes(FILE *serialfp, int readrawcount, int offset)
+{
+	char replybuffer[REPLY_SIZE];
+	int bytesread = 0;
+
+	fprintf(serialfp, "reset\r");
+	fgets(replybuffer, REPLY_SIZE, serialfp);
+	stripnl(replybuffer);
+	checkfordone("reset", replybuffer);
+
+	fprintf(serialfp, "clock %d\r", offset);
+	fgets(replybuffer, REPLY_SIZE, serialfp);
+	stripnl(replybuffer);
+	checkfordone("clock", replybuffer);	
 	
-	return (0);
-}
-
-void stripnl(char *s)
-{
-	char *t = s;
-
-	while (*t != '\r' && *t != '\n') t++;
-	*t = '\0';
-}
-
-void checkfordone(char *command, char *replybuffer)
-{
-	if (strcmp(replybuffer, "Done") != 0)
+	fprintf(serialfp, "dumpraw %d\r", readrawcount);
+	while (bytesread < readrawcount)
 	{
-		fprintf(stderr, "Invalid response to %s command (%s)\n", command, replybuffer);
-		exit(1);
+		unsigned char memorybyte;
+		
+		if (fread(&memorybyte, 1, 1, serialfp) < 1)
+		{
+			perror("Unable to read serial when validating");
+			return 1;
+		}
+		
+		fwrite(&memorybyte, 1, 1, stdout);
+
+		bytesread++;
 	}
+
+	return 0;
+}
+
+void usage(char *argv0)
+{
+	fprintf(stderr, "Usage: %s (-r size [-o offset])|-f filename [-s serialport]\n" \
+		"\tserialport defaults to /dev/ttyUSB0\n", argv0);
+	exit(1);
 }
